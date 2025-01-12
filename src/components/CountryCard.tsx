4dd62f5d1c7fb2cd3461e6a0ff0beaa1828c958e
umutplvn <umutpehlivan2078@gmail.com>
2025-01-12 06:50:54 +0000

    up

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CountryType } from '../types'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -12,17 +12,18 @@ interface ICountryProps {
 const CountryCard: React.FC<ICountryProps> = ({country}) => {
 console.log(country);
 
+const [hover, setHover] = useState<Boolean>(false)
   
   return (
-    <div>
-       <Card style={{ width: '18rem', height:"20rem" }}>
+       <Card style={{ width: '18rem', height:"20rem", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", cursor:"pointer", transition:"1s ease-in-out" }}
+       onMouseEnter={()=>setHover(true)}
+       >
       <Card.Body>
       <Card.Img style={{height:"10rem"}} variant="top" src={country.flags.png} />
         <Card.Title >{country.name.official}</Card.Title>
         <Button style={{position:"absolute", bottom:"0.5rem", left:"0.5rem", backgroundColor:"#7f7f7f", border:"none"}} >Details</Button>
       </Card.Body>
     </Card>
-    </div>
   )
 }
 
