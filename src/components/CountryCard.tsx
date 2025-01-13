28b0dce7dfc99cdf0f046661880f6a9fc1ccec44
umutplvn <umutpehlivan2078@gmail.com>
2025-01-13 01:12:57 +0000

    up

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,29 +1,48 @@
 import React, { useState } from 'react'
 import { CountryType } from '../types'
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
 interface ICountryProps {
-    country: CountryType
+  country: CountryType
 }
 
 
-const CountryCard: React.FC<ICountryProps> = ({country}) => {
-console.log(country);
+const CountryCard: React.FC<ICountryProps> = ({ country }) => {
+  console.log(country);
 
-const [hover, setHover] = useState<Number>(1)
-  
+  const [hover, setHover] = useState<Number>(1)
+  const [link, setLink] = useState<String>("none")
+
+
+  const HoverFunc = () => {
+    setHover(1.02)
+    setLink("block")
+  }
+
+  const RemoveHoverFunc = () => {
+    setLink("none")
+    setHover(1)
+  }
+
   return (
-       <Card style={{ width: '18rem', height:"20rem", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", cursor:"pointer", transition:"0.2s ease-in-out", transform:`scale(${hover})` }}
-       onMouseEnter={()=>setHover(1.02)}
-       onMouseLeave={()=>setHover(1)}
-       >
-      <Card.Body>
-      <Card.Img style={{height:"10rem"}} variant="top" src={country.flags.png} />
-        <Card.Title style={{width:"100%", position:"absolute", bottom:"2rem", left:"0", textAlign:"center", fontFamily:"sans-serif", fontWeight:"600", height:"3rem"}} >{country.name.official}</Card.Title>
+    <Card style={{ width: '18rem', height: "20rem", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", cursor: "pointer", transition: "0.2s ease-in-out", transform: `scale(${hover})` }}
+      onMouseEnter={HoverFunc}
+      onMouseLeave={RemoveHoverFunc}
 
+    >
+      <Card.Body>
+        <Card.Img style={{ height: "10rem" }} variant="top" src={country.flags.png} />
+        <Card.Title style={{ width: "100%", position: "absolute", bottom: "2rem", left: "0", textAlign: "center", fontFamily: "sans-serif", fontWeight: "600", height: "3rem" }} >{country.name.official}</Card.Title>
       </Card.Body>
+
+
+      <i className="bi bi-box-arrow-up-right" style={{ fontSize: "1.2rem", padding: "0.5rem", display: `${link}`}}
+
+
+      ></i>
+
+
     </Card>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const rootElement = document.querySelector('#root');
 if (!rootElement) {
