05c0735ee57205881d803c60d9e8ab5303d5120d
umutplvn <umutpehlivan2078@gmail.com>
2025-01-13 00:44:10 +0000

    styling

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -12,16 +12,17 @@ interface ICountryProps {
 const CountryCard: React.FC<ICountryProps> = ({country}) => {
 console.log(country);
 
-const [hover, setHover] = useState<Boolean>(false)
+const [hover, setHover] = useState<Number>(1)
   
   return (
-       <Card style={{ width: '18rem', height:"20rem", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", cursor:"pointer", transition:"1s ease-in-out" }}
-       onMouseEnter={()=>setHover(true)}
+       <Card style={{ width: '18rem', height:"20rem", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", cursor:"pointer", transition:"0.2s ease-in-out", transform:`scale(${hover})` }}
+       onMouseEnter={()=>setHover(1.02)}
+       onMouseLeave={()=>setHover(1)}
        >
       <Card.Body>
       <Card.Img style={{height:"10rem"}} variant="top" src={country.flags.png} />
-        <Card.Title style={{padding:"0.5rem 0 0 0", textAlign:"center", fontFamily:"sans-serif", fontWeight:"600"}} >{country.name.official}</Card.Title>
-        <Button style={{position:"absolute", bottom:"1rem",backgroundColor:"#7f7f7f", border:"none"}} >Details</Button>
+        <Card.Title style={{width:"100%", position:"absolute", bottom:"2rem", left:"0", textAlign:"center", fontFamily:"sans-serif", fontWeight:"600", height:"3rem"}} >{country.name.official}</Card.Title>
+
       </Card.Body>
     </Card>
   )
